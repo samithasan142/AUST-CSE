05347dfac0192058f289db2c41970e2c22c29767
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2022-05-30 11:59:12 +0600

    Add files via upload

---
+++ CSE 3.2/CSE 3200 (SD-V)/180204142_Assignment-1/Assignment1_B2/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Enter a string for first List: ");
+String names = Console.ReadLine();
+var firstList = new List<string>();
+
+//Apple Red Purple Pineapple Yellow Blue
+
+
+string[] words = names.Split(' ');
+foreach (var word in words)
+{
+
+    firstList.Add(word);
+}
+
+Console.WriteLine("\nAll Names in First list:\n");
+foreach (var fruits in firstList)
+{
+    Console.WriteLine(fruits);
+}
+//Q(b)
+
+Console.WriteLine("\nEnter a string for Second List: ");
+String names2 = Console.ReadLine();
+var secondList = new List<string>();
+
+//Black Silver Mango
+
+string[] words2 = names2.Split(' ');
+foreach (var word in words2)
+{
+
+    secondList.Add(word);
+}
+
+Console.WriteLine("\nAll Names in Second list:\n");
+foreach (var fruits in secondList)
+{
+    Console.WriteLine(fruits);
+}
+//Q(c)
+
+secondList.InsertRange(0, firstList);
+Console.WriteLine("\nAfter inserting First List on top of the second list :\n");
+
+foreach (var fruits in secondList)
+{
+    Console.WriteLine(fruits);
+}
+
+//Q(d)
+Console.WriteLine("\nAfter Discarding  names and keep color names: \n");
+secondList.Remove("Apple");
+secondList.Remove("Pineapple");
+secondList.Remove("Mango");
+
+foreach (var fruits in secondList)
+{
+    Console.WriteLine(fruits);
+}
+
+Console.WriteLine("\n");
+
+//Q(e)
+Console.WriteLine("\nAfter converting all the names of second list into Upper Case :\n");
+foreach (var country in secondList)
+{
+    Console.Write(country.ToUpper() + "\n");
+}
---
+++ CSE 3.2/CSE 3200 (SD-V)/180204142_Assignment-1/Assignment2_B2/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int row1, col1, row2, col2, i, j;
+
+Console.Write("Enter Row-1 Number : ");
+row1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Column-1 Number : ");
+col1 = Convert.ToInt32(Console.ReadLine());
+
+
+double[,] arr1 = new double[row1, col1];
+
+
+Console.WriteLine("Insert a 2D matrix-1: ");
+
+for (i = 0; i < row1; i++)
+{
+    for (j = 0; j < col1; j++)
+    {
+        Console.Write("insert  [{0},{1}] : ", i, j);
+        arr1[i, j] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+
+Console.Write("Enter Row2 Number : ");
+row2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Column-2 Number : ");
+col2 = Convert.ToInt32(Console.ReadLine());
+double[,] arr2 = new double[row2, col2];
+
+Console.WriteLine("Insert a 2D matrix-2: ");
+
+for (i = 0; i < row2; i++)
+{
+    for (j = 0; j < col2; j++)
+    {
+        Console.Write("insert  [{0},{1}] : ", i, j);
+        arr2[i, j] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+Console.Write("Input matrix-1 is : \n");
+for (i = 0; i < row1; i++)
+{
+    for (j = 0; j < col1; j++)
+    {
+        Console.Write("{0}\t", arr1[i, j]);
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input matrix-2 is : \n");
+for (i = 0; i < row2; i++)
+{
+    for (j = 0; j < col2; j++)
+    {
+        Console.Write("{0}\t", arr2[i, j]);
+    }
+    Console.WriteLine();
+}
+
+if (col1 != row2)
+{
+    Console.WriteLine("Invalid Input (cannot be multiplied)");
+    Console.Read();
+}
+else
+{
+    double[,] c = new double[row1, col2];
+    for (i = 0; i < row1; i++)
+    {
+        for (j = 0; j < col2; j++)
+        {
+            c[i, j] = 0;
+            for (int k = 0; k < col1; k++)
+            {
+                c[i, j] += arr1[i, k] * arr2[k, j];
+            }
+        }
+    }
+    Console.WriteLine("The product of the two matrices is :");
+    for (i = 0; i < row1; i++)
+    {
+        for (j = 0; j < col1; j++)
+        {
+            Console.Write(c[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.Read();
+}
